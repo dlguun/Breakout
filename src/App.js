cbb525b73b3b7bbca898aaf6d4d559b023c5c019
dlguun <mduk96@gmail.com>
2021-05-23 14:54:59 +0800

    ver1

--- src/App.js
+++ src/App.js
@@ -295,7 +295,7 @@ const Breakout = () => {
 
     return (
         <div className='w-screen h-screen flex items-center text-white' style={{ backgroundColor: '#263445', cursor: `${ !isOver ? 'none' : 'default' }` }}>
-            <div className='relative mx-auto my-auto flex flex-col border-2 border-white' style={{ minWidth: '600px', minHeight: '600px'}}>
+            <div className='relative mx-auto my-auto w-full h-full flex flex-col border-2 border-white' style={{ maxWidth: '600px', maxHeight: '600px'}}>
                 <header className='px-2 py-4 w-full flex'>
                     <span className='flex-1'>Score: { score }</span>
                     <div className='flex gap-2'>{ lifes.map((item, index) => <div key={ index } className='w-6 h-3 bg-red-500 rounded-sm'></div>) }</div>
@@ -310,7 +310,7 @@ const Breakout = () => {
                 </div>
                 <div ref={ ballRef } className='absolute rounded-full bg-red-500' style={{ width: '5%', height: '5%', top: `calc(${ ball.top }px - 5%)`, left: `calc(${ ball.left }px - 5%)` }}></div>
                 <div className='absolute bottom-0 w-1/4 h-5' style={{ backgroundColor: '#2c8bd5', left: `${ board }px` }}></div>
-                { isOver && <button onClick={ restart } className='absolute top-2 left-60 z-2 px-4 py-2 border-2 border-white focus:outline-none' style={{ backgroundColor: '#263445' }}>Game Over!</button> }
+                { isOver && <button onClick={ restart } className='absolute top-2 z-2 px-4 py-2 border-2 border-white focus:outline-none' style={{ backgroundColor: '#263445', left:'calc(50% - 121px / 2)' }}>Game Over!</button> }
             </div>
         </div>
     );
